f156cd3ea381858635516cfd0fe0d4250b709855
Unknown <meltoz@outlook.fr>
2018-01-13 17:12:44 +0100

    Creating project
    Adding main class on project

---
+++ src/com/discord/bot/blindtest/launcher/Launcher.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.discord.bot.blindtest.launcher;
+
+/**
+ * @author Meltoz
+ *
+ */
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
